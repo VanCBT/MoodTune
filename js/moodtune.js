2e442c8d66501751cd45cab62641068e1d54b06a
Vanessa BT <vanessa@pilyr.io>
2024-08-10 05:03:04 -0400

    need to find what is  overriding so that our click button works

--- js/moodtune.js
+++ js/moodtune.js
@@ -9,26 +9,6 @@ const Next2 = document.getElementById("next2");
 const Next3 = document.getElementById("next3");
 const Next4 = document.getElementById("next4");
 
-Next1.onclick = function() {
-  Form1.style.left = "-450px";
-  Form2.style.left = "40px";
-}
-
-Next2.onclick = function() {
-  Form2.style.left = "-450px";
-  Form3.style.left = "40px";
-}
-
-Next3.onclick = function() {
-  Form3.style.left = "-450px";
-  Form4.style.left = "40px";
-}
-
-Next4.onclick = function() {
-  Form4.style.left = "-450px";
-  Form5.style.left = "40px";
-}
-
 const forms = document.querySelectorAll('fieldset');
 const nextButtons = document.querySelectorAll('.next-question');
 
