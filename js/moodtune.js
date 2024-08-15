7d01e1369f928036e8e29024464110007dd75328
Vanessa BT <vanessa@pilyr.io>
2024-08-15 12:18:50 -0400

    api to get token

--- js/moodtune.js
+++ js/moodtune.js
@@ -1,3 +1,21 @@
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+async function getToken() {
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: new URLSearchParams({
+      'grant_type': 'client_credentials',
+    }),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64')),
+    },
+    });
+
+    return await response.json();
+}
+
 const Form1 = document.getElementById("Form1");
 const Form2 = document.getElementById("Form2");
 const Form3 = document.getElementById("Form3");
